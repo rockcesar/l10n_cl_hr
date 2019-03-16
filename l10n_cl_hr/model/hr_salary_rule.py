1fdb17a6988ceb0764973263f8c1491d3d7b6818
rockcesar <rockcesar@gmail.com>
2019-03-16 20:28:39 -0300

    Add files via upload

--- l10n_cl_hr/model/hr_salary_rule.py
+++ l10n_cl_hr/model/hr_salary_rule.py
@@ -20,35 +20,23 @@ class HrSalaryRule(models.Model):
         """
         self.ensure_one()
         if self.amount_select == 'fix':
-            return self.amount_fix, float(safe_eval(self.quantity, localdict)), 100.0
-            """
             try:
                 return self.amount_fix, float(safe_eval(self.quantity, localdict)), 100.0
-            except:
-                raise UserError(_('Wrong quantity defined for salary rule %s (%s).') % (self.name, self.code))
-            """
+            except Exception as e:
+                raise UserError(_('Wrong quantity defined for salary rule %s (%s). Error: %s.') % (self.name, self.code, e))
         elif self.amount_select == 'percentage':
-            return (float(safe_eval(self.amount_percentage_base, localdict)),
-                        float(safe_eval(self.quantity, localdict)),
-                        self.amount_percentage)
-            """
             try:
                 return (float(safe_eval(self.amount_percentage_base, localdict)),
                         float(safe_eval(self.quantity, localdict)),
                         self.amount_percentage)
-            except:
-                raise UserError(_('Wrong percentage base or quantity defined for salary rule %s (%s).') % (self.name, self.code))
-            """
+            except Exception as e:
+                raise UserError(_('Wrong percentage base or quantity defined for salary rule %s (%s). Error: %s.') % (self.name, self.code, e))
         else:
-            safe_eval(self.amount_python_compute, localdict, mode='exec', nocopy=True)
-            return float(localdict['result']), 'result_qty' in localdict and localdict['result_qty'] or 1.0, 'result_rate' in localdict and localdict['result_rate'] or 100.0
-            """
             try:
                 safe_eval(self.amount_python_compute, localdict, mode='exec', nocopy=True)
                 return float(localdict['result']), 'result_qty' in localdict and localdict['result_qty'] or 1.0, 'result_rate' in localdict and localdict['result_rate'] or 100.0
-            except:
-                raise UserError(_('Wrong python code defined for salary rule %s (%s).') % (self.name, self.code))
-            """
+            except Exception as e:
+                raise UserError(_('Wrong python code defined for salary rule %s (%s). Error: %s.') % (self.name, self.code, e))
 
     @api.multi
     def _satisfy_condition(self, localdict):
@@ -61,22 +49,14 @@ class HrSalaryRule(models.Model):
         if self.condition_select == 'none':
             return True
         elif self.condition_select == 'range':
-            result = safe_eval(self.condition_range, localdict)
-            return self.condition_range_min <= result and result <= self.condition_range_max or False
-            """
             try:
                 result = safe_eval(self.condition_range, localdict)
                 return self.condition_range_min <= result and result <= self.condition_range_max or False
-            except:
-                raise UserError(_('Wrong range condition defined for salary rule %s (%s).') % (self.name, self.code))
-            """
+            except Exception as e:
+                raise UserError(_('Wrong range condition defined for salary rule %s (%s). Error: %s.') % (self.name, self.code, e))
         else:  # python code
-            safe_eval(self.condition_python, localdict, mode='exec', nocopy=True)
-            return 'result' in localdict and localdict['result'] or False
-            """
             try:
                 safe_eval(self.condition_python, localdict, mode='exec', nocopy=True)
                 return 'result' in localdict and localdict['result'] or False
-            except:
-                raise UserError(_('Wrong python condition defined for salary rule %s (%s).') % (self.name, self.code))
-            """
+            except Exception as e:
+                raise UserError(_('Wrong python condition defined for salary rule %s (%s). Error: %s.') % (self.name, self.code, e))
