2b6b98f50f1124920f21abcddab1f528aae2010f
rockcesar <rockcesar@gmail.com>
2019-03-16 20:01:08 -0300

    Add files via upload

--- l10n_cl_hr/model/hr_salary_rule.py
+++ l10n_cl_hr/model/hr_salary_rule.py
@@ -1,9 +1,43 @@
-from odoo import api, fields, models, tools, _
+# -*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.safe_eval import safe_eval
 
-class hr_salary_rule(models.Model):
-    _inherit = 'hr.salary.rule'
-    _description = 'Salary Rule'
-    
-    date_start = fields.Date('Fecha Inicio',  help="Fecha de inicio de la regla salarial")
-    date_end = fields.Date('Fecha Fin',  help="Fecha del fin de la regla salarial")
+from odoo.addons import decimal_precision as dp
+
+class HrSalaryRule(models.Model):
+    _inherit = ['hr.salary.rule']
+
+    #TODO should add some checks on the type of result (should be float)
+    @api.multi
+    def _compute_rule(self, localdict):
+        """
+        :param localdict: dictionary containing the environement in which to compute the rule
+        :return: returns a tuple build as the base/amount computed, the quantity and the rate
+        :rtype: (float, float, float)
+        """
+        self.ensure_one()
+        if self.amount_select == 'fix':
+            try:
+                return self.amount_fix, float(safe_eval(self.quantity, localdict)), 100.0
+            except:
+                raise UserError(_('Wrong quantity defined for salary rule %s (%s).') % (self.name, self.code))
+        elif self.amount_select == 'percentage':
+            try:
+                return (float(safe_eval(self.amount_percentage_base, localdict)),
+                        float(safe_eval(self.quantity, localdict)),
+                        self.amount_percentage)
+            except:
+                raise UserError(_('Wrong percentage base or quantity defined for salary rule %s (%s).') % (self.name, self.code))
+        else:
+            safe_eval(self.amount_python_compute, localdict, mode='exec', nocopy=True)
+            return float(localdict['result']), 'result_qty' in localdict and localdict['result_qty'] or 1.0, 'result_rate' in localdict and localdict['result_rate'] or 100.0
+            """
+            try:
+                safe_eval(self.amount_python_compute, localdict, mode='exec', nocopy=True)
+                return float(localdict['result']), 'result_qty' in localdict and localdict['result_qty'] or 1.0, 'result_rate' in localdict and localdict['result_rate'] or 100.0
+            except:
+                raise UserError(_('Wrong python code defined for salary rule %s (%s).') % (self.name, self.code))
+            """
